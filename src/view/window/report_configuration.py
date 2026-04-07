cd6c6508798bdf4c38dc85eb46f81fc7c27f3205
Martin <martinpop06@gmail.com>
2026-04-07 20:18:29 +0200

    gui style update

--- src/view/window/report_configuration.py
+++ src/view/window/report_configuration.py
@@ -15,6 +15,25 @@ def _show_invalid_ip_message():
     msg.setStandardButtons(QMessageBox.StandardButton.Ok)
     msg.exec()
 
+class LabelWithValue(QWidget):
+    def __init__(self, label, value):
+        super().__init__()
+        self._label = label
+        self._value = value
+        self._init_ui()
+
+    def _init_ui(self):
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(5)
+
+        lbl_title = QLabel(self._label)
+        lbl_value = QLabel(self._value)
+        lbl_value.setObjectName("value_label")
+
+        layout.addWidget(lbl_title)
+        layout.addWidget(lbl_value)
+        layout.addStretch()
 
 class ReportConfigurationWindow(QWidget):
     closed_signal = Signal()
@@ -30,20 +49,21 @@ class ReportConfigurationWindow(QWidget):
         event.accept()
 
     def _init_ui(self):
-        self.setWindowTitle("Report Configuration")
+        self.setWindowTitle(f"Report Configuration - {self._file_info.get('name')}")
         self.resize(450, 350)
+        self.setObjectName("background")
 
         main_layout = QVBoxLayout(self)
         main_layout.setContentsMargins(15, 15, 15, 15)
         main_layout.setSpacing(10)
 
         # file name
-        self.lbl_filename = QLabel(f"File: {self._file_info.get('name')}")
-        main_layout.addWidget(self.lbl_filename)
+        file_name_lbl = LabelWithValue("File Name:", self._file_info.get("name"))
+        main_layout.addWidget(file_name_lbl)
 
         # file size
-        self.lbl_size = QLabel(f"Size: {self._file_info.get('size')}")
-        main_layout.addWidget(self.lbl_size)
+        lbl_size = LabelWithValue(f"Size:", self._file_info.get('size'))
+        main_layout.addWidget(lbl_size)
 
         # saved captures
         saved_captures = self._file_info.get('saved_captures')
@@ -53,19 +73,19 @@ class ReportConfigurationWindow(QWidget):
             cache_frame.setObjectName("background_frame")
             cache_layout = QVBoxLayout(cache_frame)
 
-            lbl_status = QLabel("Already processed for:")
+            lbl_status = QLabel("Saved analysed captures:")
             cache_layout.addWidget(lbl_status)
 
             for k, v in saved_captures.items():
-                lbl_item = QLabel(f"• IP: {k}  |  Date: {v[1]}")
-                lbl_item.setObjectName("lbl_item")
+                lbl_item = QLabel(f"• IP: {k} - {v[1].strftime('%Y-%m-%d %H:%M:%S')}")
+                lbl_item.setObjectName("value_label")
                 cache_layout.addWidget(lbl_item)
+
+            main_layout.addWidget(cache_frame)
 
             self.checkbox = QCheckBox("Force new analysis")
             self.checkbox.setObjectName("chk_reanalyze")
-            cache_layout.addWidget(self.checkbox)
-
-            main_layout.addWidget(cache_frame)
+            main_layout.addWidget(self.checkbox)
 
         # target ip
         ip_layout = QVBoxLayout()
@@ -74,7 +94,6 @@ class ReportConfigurationWindow(QWidget):
         lbl_ip_input = QLabel("Target IP address:")
 
         self.input_ip = QLineEdit()
-        self.input_ip.setObjectName("input_ip")
         self.input_ip.setPlaceholderText("192.168.1.50")
 
         ip_layout.addWidget(lbl_ip_input)
